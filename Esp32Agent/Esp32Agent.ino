cf9a81a78adcc5d8e5c8512d75c3066fb1fb4d59
imigelose123 <101578354+imigelose123@users.noreply.github.com>
2024-01-05 10:32:14 -0400

    adding esp32 file

---
+++ Esp32Agent/Esp32Agent.ino
@@ -0,0 +1,20 @@
+const int LED_BUILTIN = 25;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    int on = Serial.parseInt();
+
+    if (on == 1) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+
+    if (on == 2) {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  }
+}
