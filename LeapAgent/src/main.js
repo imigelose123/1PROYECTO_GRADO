3d4e9630e25970b7d5514c9ed897d60ca1ffcb3c
imigelose123 <101578354+imigelose123@users.noreply.github.com>
2024-05-26 20:46:13 -0400

    refactorisando

--- LeapAgent/src/main.js
+++ LeapAgent/src/main.js
@@ -7,6 +7,34 @@ let estado = "2"
 
 const miguel = (frame) => {
   return {
+    hand: function (hand) {
+      const pulgar = hand.fingers[0];
+      const indice = hand.fingers[1];
+      const medio = hand.fingers[2];
+      const anular = hand.fingers[3];
+      const menique = hand.fingers[4];
+  
+      const distancia = Leap.vec3.distance(
+        pulgar.dipPosition,
+        indice.dipPosition
+      );
+  
+      if (distancia < 35) {
+          console.log("presionado");
+        if (estado=='2') {
+          serialport.write('1')
+          estado = '1'
+        }
+  
+      } else {
+        console.log("apado");
+        if (estado=='1') {
+          serialport.write('2')
+          estado = '2'
+        }
+      }
+    },
+
     frame: (frame) => {
       if (frame.hands.length > 0) {
         //  this.emit("handFound");
@@ -17,33 +45,4 @@ const miguel = (frame) => {
 };
 
 Leap.Controller.plugin("miguel", miguel);
-
-Leap.loop({
-  hand: function (hand) {
-    const pulgar = hand.fingers[0];
-    const indice = hand.fingers[1];
-    const medio = hand.fingers[2];
-    const anular = hand.fingers[3];
-    const menique = hand.fingers[4];
-
-    const distancia = Leap.vec3.distance(
-      pulgar.dipPosition,
-      indice.dipPosition
-    );
-
-    if (distancia < 35) {
-        console.log("presionado");
-      if (estado=='2') {
-        serialport.write('1')
-        estado = '1'
-      }
-
-    } else {
-      console.log("apado");
-      if (estado=='1') {
-        serialport.write('2')
-        estado = '2'
-      }
-    }
-  }
-}).use("miguel");
+Leap.loop({}).use("miguel");
